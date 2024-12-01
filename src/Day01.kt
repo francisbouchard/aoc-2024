7c2a152ab386e48640b77e2640f3b902da6c9b41
Francis Bouchard <francis.bouchard91@gmail.com>
2024-12-01 17:06:00 -0500

    solution to day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,28 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+
+    input.forEach { line ->
+        val values = line.split("   ").map { it.toInt() }
+        list1.add(values[0])
+        list2.add(values[1])
+    }
+
+    list1.sort()
+    list2.sort()
+
+    val solution1 = list1.mapIndexed { index, i -> abs(i-list2[index]) }.sum()
+
+    val solution2 = list1.sumOf { i ->
+        i * list2.count { i == it }
+    }
+
+    println(solution1)
+    println(solution2)
 }
